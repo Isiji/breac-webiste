3f23e147f1ac45989bdf0cf22834bae7fd011306
Isiji <blairisiji@gmail.com>
2025-03-24 14:50:29 +0300

    reverting to browser router to see if it will work

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter } from "react-router-dom"; // Import HashRouter
+import { BrowserRouter } from "react-router-dom"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter> 
+    <BrowserRouter basename="/"> 
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
 reportWebVitals();
